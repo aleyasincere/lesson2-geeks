bcc79f9f59ff86256fa581e436c90cd0e0072200
Aleyasincere <abdrasulovaliya.pi9.8@gmail.com>
2024-05-17 17:53:52 +0600

    hw1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const Title = () => {
+    return (
+        <div>
+            title
+        </div>
+    );
+};
+
+
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        Header
+          <Title />
+      </div>
   );
-}
+};
+
+
+const Content = () => {
+  return (
+      <div>
+        Content
+          <Title/>
+      </div>
+  );
+};
+
+
+const Footer = () => {
+  return (
+      <div>
+          Footer
+         <Title/>
+      </div>
+  );
+};
+
+
+
+const App = () => {
+    return (
+        <div>
+          <Header />
+          <Content />
+          <Footer />
+        </div>
+    );
+};
 
 export default App;
